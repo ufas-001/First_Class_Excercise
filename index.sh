db68efc0ca88aea8e3c90418c5012cc9b1cda3e3
ufas-001 <fasabdulmutolib@gmail.com>
2023-03-12 09:33:21 +0100

    medium excercise

--- index.sh
+++ index.sh
@@ -3,4 +3,33 @@ name="John"
 echo "My name is ${name}"
 name="Joe"
 echo "My name is ${name}"
+## Exercise 1
+echo "Bash scripting is fun"
+## Excercise 2
+name="Bash scripting is fun"
+echo $name
+## Excercise 3
+HOSTNAME=$(hostname)
+echo "This script is running on $HOSTNAME"
+## Excercise 4
+## The first condition checks if the file exit and it echos the file is passworded, the second condition checks if the user has an editing permission
+FILE="/home/hafeez"
+
+if [ -e "$FILE" ]
+    then
+        echo "$FILE is password enabled"
+    else
+        echo "$FILE doesn't exist"
+fi
+
+if [ -x "$FILE" ]
+    then
+        echo "You have permission to edit $FILE"
+    else
+        echo "You don't have permission to edit $FILE"
+fi
+
+## Excercise 5
+
+ANIMAL="man bear pig dog cat sheep"
 
